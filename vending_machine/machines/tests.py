261373e344225aec21915fbe0849165b4edb6ff9
joaquin <juaco1718@gmail.com>
2020-11-14 01:59:29 -0300

    fully tested put one coin feature

--- vending_machine/machines/tests.py
+++ vending_machine/machines/tests.py
@@ -18,6 +18,15 @@ class MachineModelTest(TestCase):
         first_machine.save()
         self.assertEqual(first_machine.coins, 0)
 
+    def test_add_one_coin(self):
+        first_machine = Machine.objects.add_one_coin()
+        self.assertEqual(first_machine.coins, 1)
+
+    def test_add_two_coins(self):
+        Machine.objects.add_one_coin()
+        first_machine = Machine.objects.add_one_coin()
+        self.assertEqual(first_machine.coins, 2)
+
 
 class VendingMachineAPITest(TestCase):
 
@@ -26,8 +35,20 @@ class VendingMachineAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['content-type'], 'application/json')
 
-    def test_post_add_one_coin(self):
+    def test_put_204_and_json_content(self):
         response = self.client.put('/')
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response['content-type'], 'application/json')
-        self.assertEqual(response['X-Coins'], '1')
+
+    def test_put_one_coin(self):
+        self.client.put('/')
+        self.assertEqual(Machine.objects.count(), 1)
+        new_machine = Machine.objects.first()
+        self.assertEqual(new_machine.coins, 1)
+
+    def test_put_twice(self):
+        self.client.put('/')
+        self.client.put('/')
+        self.assertEqual(Machine.objects.count(), 1)
+        new_machine = Machine.objects.first()
+        self.assertEqual(new_machine.coins, 2)
