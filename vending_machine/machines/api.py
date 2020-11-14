a8623ef27fb1f54010da561162413bf56358a7ba
joaquin <juaco1718@gmail.com>
2020-11-14 17:15:23 -0300

    green status test for delete coins

--- vending_machine/machines/api.py
+++ vending_machine/machines/api.py
@@ -6,8 +6,14 @@ import json
 def add_one_coin():
     return Machine.objects.add_one_coin()
 
+def delete_coins():
+    return Machine.objects.delete_coins()
+
 
 def home(request):
+    if request.method == 'GET':
+        return HttpResponse('<html><title>Vending Machine</title></html>')
+
     if request.method == 'PUT':
         machine = add_one_coin()
         response = HttpResponse(status=204,
@@ -16,7 +22,13 @@ def home(request):
         response['X-Coins'] = machine.coins
         return response
 
-    elif request.method == 'GET':
-        return HttpResponse(content_type='application/json')
+    if request.method=='DELETE':
+        coins_returned = delete_coins()
+        response = HttpResponse(status=204,
+                                content_type='application/json'
+                                )
+        response['X-Coins'] = coins_returned
+        return response
+
 
 
