f1a2b0f53431bdcd0ae4b9eb57a07ce0bb8a383a
joaquin <juaco1718@gmail.com>
2020-11-18 00:19:23 -0300

    green status test for buy a certain item

--- vending_machine/machines/api.py
+++ vending_machine/machines/api.py
@@ -66,9 +66,33 @@ def inventory(request, id=None):
                                         content_type='application/json'
                                         )
                 return response
-    response = HttpResponse(status=404)
-    return response
 
+    if request.method == 'PUT' and id is not None:
+        machine = Machine.objects.first()
+        if machine is not None:
+            if machine.beverageitem_set.filter(id=id).exists():
+                item = machine.beverageitem_set.get(id=id)
+                if item.quantity > 0 and item.price <= machine.coins:
+                    Machine.objects.buy_item(item.pk)
+                    response = HttpResponse(json.dumps({"quantity":1}),
+                                            status=200,
+                                            content_type='application/json'
+                                            )
+                    item = machine.beverageitem_set.get(id=id)
+                    machine = Machine.objects.first()
+                    response['X-Coins'] = machine.coins
+                    response['X-Inventory-Remaining'] = item.quantity
+                    return response
+                if item.price > machine.coins:
+                    response = HttpResponse(status=400)
+                    response['X-Coins'] = machine.coins
+                    return response
+            response = HttpResponse(status=404)
+            response['X-Coins'] = machine.coins
+            return response
+    response = HttpResponse(status=404)
+    response['X-Coins'] = 0 #machine does not exist
+    return response
 
 def refill(request):
     if request.method == 'POST':
