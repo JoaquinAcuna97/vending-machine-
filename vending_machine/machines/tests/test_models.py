c8e4c820fc8f3528a869758b6387a7c394caf07d
joaquin <juaco1718@gmail.com>
2020-11-17 23:06:13 -0300

    red status tests for buy a certain item models

--- vending_machine/machines/tests/test_models.py
+++ vending_machine/machines/tests/test_models.py
@@ -100,3 +100,63 @@ class RelationItemMachine(TestCase):
         self.assertEqual(machine.beverageitem_set.count(), 3)
 
 
+class BuyAItem(TestCase):
+
+    def test_buy_one_item_success_no_remaining_coins(self):
+        machine = Machine(coins=2)
+        machine.save()
+        beer = Item(name='Beer', vending_machine=machine)
+        beer.save()
+        remaining_items = Machine.objects.buy_item(beer.id)
+        self.assertEqual(remaining_items, 4)
+        self.assertEqual(machine.coins, 0)
+
+    def test_buy_one_item_success_one_remaining_coin(self):
+        machine = Machine(coins=3)
+        machine.save()
+        beer = Item(name='Beer', vending_machine=machine)
+        beer.save()
+        remaining_items = Machine.objects.buy_item(beer.id)
+        self.assertEqual(remaining_items, 4)
+        self.assertEqual(machine.coins, 1)
+
+    def test_buy_two_times_one_item_success(self):
+        machine = Machine(coins=4)
+        machine.save()
+        beer = Item(name='Beer', vending_machine=machine)
+        beer.save()
+        Machine.objects.buy_item(beer.id)
+        remaining_items = Machine.objects.buy_item(beer.id)
+        self.assertEqual(remaining_items, 3)
+        self.assertEqual(machine.coins, 0)
+
+    def test_buy_two_different_items_success(self):
+        machine = Machine(coins=4)
+        machine.save()
+        beer = Item(name='Beer', vending_machine=machine)
+        beer.save()
+        lemonade = Item(name='Lemonade', vending_machine=machine)
+        lemonade.save()
+        Machine.objects.buy_item(beer.id)
+        remaining_items = Machine.objects.buy_item(lemonade.id)
+        self.assertEqual(remaining_items, 4)
+        self.assertEqual(machine.coins, 0)
+
+
+    def test_buy_item_no_stock(self):
+        machine = Machine(coins=4)
+        machine.save()
+        beer = Item(name='Beer', vending_machine=machine, quantity=0)
+        beer.save()
+        remaining_items = Machine.objects.buy_item(beer.id)
+        self.assertEqual(remaining_items, 0)
+        self.assertEqual(machine.coins, 4)
+
+    def test_buy_item_insufficient_founds(self):
+        machine = Machine(coins=4)
+        machine.save()
+        beer = Item(name='Beer', vending_machine=machine, price=5)
+        beer.save()
+        remaining_items = Machine.objects.buy_item(beer.id)
+        self.assertEqual(remaining_items, 5)
+        self.assertEqual(machine.coins, 4)
